82328e39f1a1cb7a0e6cfc1672d6366bdd9122f6
Muhammad Faraz       -    CS120212072 <cs120212072@kust.edu.pk>
2025-07-24 17:43:33 +0500

    Updated

--- Config/Firebase.js
+++ Config/Firebase.js
@@ -1,8 +1,15 @@
-const admin = require('firebase-admin');
-require('dotenv').config();
+const admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ACCOUNT_KEY);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const encoded = process.env.FIREBASE_KEY_BASE64;
+if (!encoded) throw new Error("Missing FIREBASE_KEY_BASE64 env variable");
+
+const decoded = Buffer.from(encoded, "base64").toString("utf-8");
+const serviceAccount = JSON.parse(decoded);
+
+if (serviceAccount.private_key.includes("\\n")) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
