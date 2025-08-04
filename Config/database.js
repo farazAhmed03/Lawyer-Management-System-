d129189bf2a85c672ac3342a4912776f26797d6b
farazAhmed03 <cs120212072.kust.edu.pk@gmail.com>
2025-08-05 00:59:16 +0500

    Update database.js

--- Config/database.js
+++ Config/database.js
@@ -1,24 +1,8 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const dbConnect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database Connected Successfully!");
-    } catch (error) {
-        console.log("Error while connecting to Database", error);
-    }
-
-}
-
-module.exports = dbConnect;
-
-
-// const dbDisconnect = async () => {
-//     try {
-//         await mongoose.disconnect();
-//         console.log("Database Disconnected Successfully!");
-//     } catch (error) {
-//         console.log("Error while disconnecting to Database", error);
-//     }
-// }
-
+module.exports = db;
