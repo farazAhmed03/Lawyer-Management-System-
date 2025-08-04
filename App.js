144613e103a6d8a5968690aee81cfcece8c77833
farazAhmed03 <cs120212072.kust.edu.pk@gmail.com>
2025-08-05 00:59:45 +0500

    Update App.js

--- App.js
+++ App.js
@@ -84,7 +84,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
-  await dbConnect();
 });
 
 // Socket Handler
